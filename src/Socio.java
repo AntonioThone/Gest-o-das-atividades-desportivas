1d12bf8b5ba50321f263a068c5d60d04ecf0874e
Ray <ray.developer@gmail.com>
2024-04-11 03:03:46 +0000

    ..

---
+++ src/Socio.java
@@ -0,0 +1,16 @@
+public class Socio<T extends Pessao> { //criacao da classe java
+
+//atributos 
+  
+private String numeroContribuinte;
+private String morada;
+private String telefone;
+private String email;
+private String estatuto;
+private LocalDate dataInscricao;
+private boolean quotasEmDia;
+private List<Quota> quotasEmDivida;
+
+    // ...
+
+}
